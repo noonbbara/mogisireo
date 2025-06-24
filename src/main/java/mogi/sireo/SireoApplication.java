112bd355c7465fb4b918823ccb838ded74fee292
DESKTOP-N0Q4ELF\ys <ysysys1226@gmail.com>
2025-06-24 18:23:44 +0900

    [Initial]

---
+++ src/main/java/mogi/sireo/SireoApplication.java
@@ -0,0 +1,13 @@
+package mogi.sireo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SireoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SireoApplication.class, args);
+	}
+
+}
