7f57efb60b5dfd3baecebda1fe2c48a3763b1104
DESKTOP-N0Q4ELF\ys <ysysys1226@gmail.com>
2025-06-25 19:00:25 +0900

    [Refactor] 선언하고 사용하지 않은 항목 제거

--- Frontend/src/pages/MainPage.tsx
+++ Frontend/src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { convertGRID } from '../utils/geoUtil';
 import { calculateMosquitoIndex } from '../utils/calculateMosquitoIndex';
 import type { ForecastItem, MosquitoIndex } from '../utils/calculateMosquitoIndex';
